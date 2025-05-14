25525d9019716089c7398ba3e39f9738e70cca15
Lucas Oliveira <lucasgarcia.detec@gmail.com>
2025-05-14 09:20:11 +0100

    fixed patterns.pl

--- docker/reasoner/patterns.pl
+++ docker/reasoner/patterns.pl
@@ -13,21 +13,21 @@ pattern(["what", "is", "the", "length", "of", P], ["The", "length", "of", P, "is
 pattern(["what", "is", "the", "length", "of", _], ["This", "city", "does", "not", "exist"]).
 
 pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "population"], [C, "is", "bigger", "in", "population."]) :- query_biggest_population(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "population"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "biggest", "in", "population"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "population"], [C, "is", "smaller", "in", "population."]) :- query_smallest_population(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "population"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "smallest", "in", "population"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "area"], [C, "is", "bigger", "in", "area."]) :- query_biggest_area(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "area"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "biggest", "in", "area"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "area"], [C, "is", "smaller", "in", "area."]) :- query_smallest_area(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "area"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "smallest", "in", "area"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "length"], [C, "is", "bigger", "in", "length."]) :- query_biggest_length(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "length"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "biggest", "in", "length"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "length"], [C, "is", "smaller", "in", "length."]) :- query_smallest_length(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "length"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "smallest", "in", "length"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "accommodations"], [C, "is", "bigger", "in", "accommodations."]) :- query_most_accommodations(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "biggest", "in", "accommodations"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "biggest", "in", "accommodations"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "accommodations"], [C, "is", "smaller", "in", "accommodations."]) :- query_least_accommodations(A, B, C), check_existence(A, B), !.
-pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", "accommodations"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
+pattern(["between", _, "and", _, "what", "city", "is", "the", "smallest", "in", "accommodations"], ["One", "or", "both", of, "these", "cities", "do", "not", "exist"]).
 
 % what are the X(%) biggest cities in Portugal in population
 % what are the X(%) smallest cities in Portugal in population
@@ -40,4 +40,11 @@ pattern(["between", A, "and", B, "what", "city", "is", "the", "smallest", "in", 
 
 % in the table XPTO execute the query XYZ
 
-% create a new table X with 
+% create a new table X with {query}
+
+% biggest city in Portugal
+% smallest city in Portugal
+% most southern city in Portugal
+% most northern city in Portugal
+% most eastern city in Portugal
+% most western city in Portugal
