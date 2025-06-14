e1eb3e2f3b5979114acd5b48695824883df435c0
Lucas Oliveira <lucasgarcia.detec@gmail.com>
2025-06-14 19:33:43 +0100

    final final version

--- docker/reasoner/simple_queries.pl
+++ docker/reasoner/simple_queries.pl
@@ -1,65 +1,44 @@
 query_habitants(City, NumPeople) :-
-    sqlite_connect('LUGARES21_PORTUGAL', Conn, [mode(read)]),
-    format(atom(Query), "with result as
-                        (SELECT LUG21DESIG as place, N_INDIVIDUOS FROM LUGARES21_MAD
-                        UNION
-                        SELECT LUG21DESIG as place, N_INDIVIDUOS FROM LUGARES21_CONT
-                        UNION
-                        SELECT LUG21DESIG as place, N_INDIVIDUOS FROM LUGARES21_AC25
-                        UNION
-                        SELECT LUG21DESIG as place, N_INDIVIDUOS FROM LUGARES21_AC26)
-                        select sum(N_INDIVIDUOS) from result WHERE place = '~w' group by place;",
-                        [City]),
-    sqlite_query(Conn, Query, row(NumPeople)),
-    sqlite_disconnect(Conn).
+    query_all_tables('N_INDIVIDUOS', L),
+    atom_string(C, City),
+    findall(N, member([C, N], L), Ns),
+    sum_list(Ns, NumPeople).
 
 query_accommodations(City, NumPeople) :-
-    sqlite_connect('LUGARES21_PORTUGAL', Conn, [mode(read)]),
-    format(atom(Query), "with result as
-                        (SELECT LUG21DESIG as place, N_ALOJAMENTOS_TOTAL FROM LUGARES21_MAD
-                        UNION
-                        SELECT LUG21DESIG as place, N_ALOJAMENTOS_TOTAL FROM LUGARES21_CONT
-                        UNION
-                        SELECT LUG21DESIG as place, N_ALOJAMENTOS_TOTAL FROM LUGARES21_AC25
-                        UNION
-                        SELECT LUG21DESIG as place, N_ALOJAMENTOS_TOTAL FROM LUGARES21_AC26)
-                        select sum(N_ALOJAMENTOS_TOTAL) from result WHERE place = '~w' group by place;",
-                        [City]),
-    sqlite_query(Conn, Query, row(NumPeople)),
-    sqlite_disconnect(Conn).
+    query_all_tables('N_ALOJAMENTOS_TOTAL', L),
+    atom_string(C, City),
+    findall(N, member([C, N], L), Ns),
+    sum_list(Ns, NumPeople).
 
 query_area(City, NumPeople) :-
-    sqlite_connect('LUGARES21_PORTUGAL', Conn, [mode(read)]),
-    format(atom(Query), "with result as
-                        (SELECT LUG21DESIG as place, SHAPE_Area FROM LUGARES21_MAD
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Area FROM LUGARES21_CONT
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Area FROM LUGARES21_AC25
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Area FROM LUGARES21_AC26)
-                        select max(SHAPE_Area) from result WHERE place = '~w' group by place;",
-                        [City]),
-    sqlite_query(Conn, Query, row(NumPeople)),
-    sqlite_disconnect(Conn).
+    query_all_tables('SHAPE_Area', L),
+    atom_string(C, City),
+    findall(N, member([C, N], L), Ns),
+    max_list(Ns, NumPeople).
 
 query_length(City, NumPeople) :-
-    sqlite_connect('LUGARES21_PORTUGAL', Conn, [mode(read)]),
-    format(atom(Query), "with result as
-                        (SELECT LUG21DESIG as place, SHAPE_Length FROM LUGARES21_MAD
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Length FROM LUGARES21_CONT
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Length FROM LUGARES21_AC25
-                        UNION
-                        SELECT LUG21DESIG as place, SHAPE_Length FROM LUGARES21_AC26)
-                        select max(SHAPE_Length) from result WHERE place = '~w' group by place;",
-                        [City]),
-    sqlite_query(Conn, Query, row(NumPeople)),
-    sqlite_disconnect(Conn).
+    query_all_tables('SHAPE_Length', L),
+    atom_string(C, City),
+    findall(N, member([C, N], L), Ns),
+    max_list(Ns, NumPeople).
 
+
+
+% ----------- accessory rules -----------
 check_existence(City) :-
     query_area(City, _).
 check_existence(CityA, CityB) :-
     query_area(CityA, _),
     query_area(CityB, _).
+
+query_all_tables(Col, L) :-
+    sqlite_connect('LUGARES21_PORTUGAL', Conn, [mode(read)]),
+    findall([City, Pop],
+        (
+            member(Table, ['LUGARES21_MAD', 'LUGARES21_CONT', 'LUGARES21_AC25', 'LUGARES21_AC26']),
+            format(atom(Q), "SELECT LUG21DESIG, ~w FROM ~w;", [Col, Table]),
+            sqlite_query(Conn, Q, row(City, Pop))
+        ),
+        L
+    ),
+    sqlite_disconnect(Conn).
